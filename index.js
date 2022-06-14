9dbe15840e3c4bda6e3bb65a71b70bdb22743866
Nazneen Oomatia <naz.chini@gmail.com>
2022-06-14 20:53:39 +0200

    resolve pull conflict

---
+++ index.js
@@ -0,0 +1,26 @@
+function createRandomHex() {
+  let hexCode = "";
+  const hexValues = "0123456789abcdef";
+
+  for (let i = 0; i < 6; i++) {
+    hexCode += hexValues.charAt(Math.floor(Math.random() * hexValues.length));
+  }
+  return hexCode;
+}
+
+function generateRandomGradient() {
+  const degree = Math.floor(Math.random() * 360);
+
+  const gradient =
+    "linear-gradient(" +
+    degree +
+    "deg, " +
+    "#" +
+    createRandomHex() +
+    ", " +
+    "#" +
+    createRandomHex() +
+    ")";
+}
+
+module.exports = generateRandomGradient;
---
+++ index.test.js
@@ -0,0 +1,1 @@
+const generateRandomGradient = require("./index");
